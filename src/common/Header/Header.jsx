2f9a52e90e4eaf7a2ad0f3a695ba6a3e9af30306
Carlos <carlossabrido12@gmail.com>
2023-06-08 19:49:32 +0200

    ternaria navbar

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,64 +1,84 @@
-import React from 'react'
-import './Header.css'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react";
+import "./Header.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useNavigate } from 'react-router-dom';
-import { logout, userData } from '../../pages/UserSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
+import { logout, userData } from "../../pages/UserSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export const Header = () => {
-    const navigate= useNavigate()
-    const dataUSerRdx=useSelector(userData)
- 
-    const dispatch=useDispatch()
+  const navigate = useNavigate();
+  const dataUSerRdx = useSelector(userData);
 
-    const logMeOut = () => {
-      dispatch(logout({ credentials: {}}));
-      setTimeout(()=>{
-        navigate("/");
-      },500)
-    }
+  const dispatch = useDispatch();
 
+  const logMeOut = () => {
+    dispatch(logout({ credentials: {} }));
+    setTimeout(() => {
+      navigate("/");
+    }, 500);
+  };
 
-    return(
-      <div>
-        {!dataUSerRdx?.credentials?.token ?(<div> <Navbar className='color' collapseOnSelect expand="lg" >
-          <Container className='color'>
+  return (
+    <div>
+      {!dataUSerRdx?.credentials?.token ? (
+        <div>
+          {" "}
+          <Navbar className="color" collapseOnSelect expand="lg">
+            <Container className="color">
+              <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link onClick={() => navigate("/")}>Home</Nav.Link>
+                </Nav>
+                <Nav>
+                  <Nav.Link onClick={() => navigate("/login")}>Login</Nav.Link>
+                  <Nav.Link eventKey={2} onClick={() => navigate("/register")}>
+                    Register
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </div>
+      ) : (
+        <Navbar className="color" collapseOnSelect expand="lg">
+          <Container className="color">
             <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link  onClick={()=>navigate('/home')}>Home</Nav.Link>
+
+                {dataUSerRdx.credentials.token.role === "client" && (
+                  <>
+                    <Nav.Link onClick={() => navigate("/")}>Home</Nav.Link>
+                    <Nav.Link onClick={() => navigate("/")}>pepe</Nav.Link>
+                  </>
+                )}
+
+                {dataUSerRdx.credentials.token.role === "admin" && (
+                  <Nav.Link onClick={() => navigate("/")}>pepe</Nav.Link>
+                )}
               </Nav>
               <Nav>
-                <Nav.Link  onClick={()=>navigate('/login')}>Login</Nav.Link>
-                <Nav.Link eventKey={2}  onClick={()=>navigate('/register')}>
-                  Register
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar></div>):(<Navbar className='color' collapseOnSelect expand="lg" >
-          <Container className='color'>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link  onClick={()=>navigate('/home')}>Home</Nav.Link>
-              </Nav>
-              <Nav>
-                <Nav.Link eventKey={2}  onClick={()=>{logMeOut();navigate('/')}}>
+                <Nav.Link
+                  eventKey={2}
+                  onClick={() => {
+                    logMeOut();
+                    navigate("/");
+                  }}
+                >
                   logout
                 </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
-        </Navbar>)
-        
-}
-        </div>
-      );
-    }
+        </Navbar>
+      )}
+    </div>
+  );
+};
--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -10,7 +10,7 @@ import { Profile } from '../Profile/Profile'
 export const Body = () => {
   return (
     <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/'element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/profile' element={<Profile/>}/>
