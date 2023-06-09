c4f9d51951a10ed3b3fb5bb0870b1f51adba7cb9
Carlos <carlossabrido12@gmail.com>
2023-06-09 19:32:20 +0200

    updates in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import './Home.css'
+import { Container, Row } from 'react-bootstrap'
+import Portada from './Media/portada3.png'
+import Parrilla from './Media/parrilla.png'
+import Asfalto from './Media/asfalto.jpg'
 
 export const Home = () => {
   return (
-    <div className='homeDesing'>Home sdf</div>
+    <div className='homeDesing'>
+    <div className='start'>
+       <img className='firstImg' src={Portada} alt="" />
+    </div>
+    <div className='start'>
+       <img className='asfalto' src={Asfalto} alt="" />
+    </div>
+       
+    </div>
   )
 }
