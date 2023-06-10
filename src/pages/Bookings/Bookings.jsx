a72fee1c4aabb9056d4ae6c6dbd2f59cd41df1b1
Carlos <carlossabrido12@gmail.com>
2023-06-10 13:24:43 +0200

    updates profile seccion

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -3,15 +3,17 @@ import './Bookings.css'
 import { useSelector } from 'react-redux'
 import { userData } from '../UserSlice'
 import { bringBooking } from '../../Services/Apicalls'
+import { Col, Container, Row } from 'react-bootstrap'
 
 export const Booking = () => {
 
   const[bookings,setBookings]=useState([])
   const dataRdx= useSelector(userData)
 
-  useEffect=(()=>{
+  useEffect(()=>{
     bringBooking(dataRdx.credentials)
     .then((resultado)=>{
+      console.log(resultado,'resultado')
     setBookings(resultado)
   })
   .catch((error)=>console.log(error))
@@ -19,7 +21,37 @@ export const Booking = () => {
 
 
   return (
-    <div className='bookingDesign'></div>
+    <div className='bookingDesign'>
+      {dataRdx.credentials.token.role === 'client'? (<div className='containerBooking'>
+        {bookings.map((booking)=>(
+          <div key={booking.id}>
+    <div>{booking.client.name}</div>
+    <div>{booking.client.lastname}</div>
+    <div>{booking.start_date}</div>
+    <div>{booking.type.circuit}</div>
+    
+    </div>
+    ))}
+    
+    </div>):( 
+    <div className='adminDesign'>
+     <Container className='margin'>
+            {bookings.map((booking)=>(
+                <Row className='table' key={booking.id}>
+                    <Col>{booking.client.name}</Col>
+                    <Col>{booking.client.lastname}</Col>
+                    <Col>{booking.client.email}</Col>
+                    <Col>{booking.start_date}</Col>
+                     <Col>{booking.type.circuit}</Col>
+                    <Col></Col> 
+                </Row>
+            ))}
+     </Container>
+     </div>
+     )}
+    
+
+    </div>
 
   )
 }
